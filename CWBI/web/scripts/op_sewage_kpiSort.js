1c90879c901d1f03310d9b256bc7a98880dd3b0d
wang <1710555800@qq.com>
2016-10-01 22:03:54 +0800

    供水板块，盈利能力项目修改完成，js文件修改完成

--- CWBI/web/scripts/op_sewage_kpiSort.js
+++ CWBI/web/scripts/op_sewage_kpiSort.js
@@ -2,13 +2,13 @@
  * 根据kpi的值来获取该kpi的分类
  * @type {string[]}
  */
-var _profit = ["盈利能力","投资回报率（ROI）", "水处理毛利率", "污水处理单价", "综合电价","ProfitKpi.json"];
-var _service = ["业务增长","营业收入增长率", "人工成本利润率", "利润总额增长率","ServiceKpi.json"];
-var _operate = ["经营效率","人均产值", "人均净利润","OperatingKpi.json"];
-var _cash = ["现金获取","经营性现金流量净额", "水费回收率", "应收账款余额增减率","GetCashKpi.json"];
-var _debt = ["债务风险","资产负债率", "已获利息倍数","DebtRiskKpi.json"];
-var _cost = ["成本控制","管理费用成本比", "人均可控管理费用", "吨水经营成本", "吨水人工成本", "单位药剂费", "单位电耗", "吨水电费", "吨水维修费", "吨水管理费","CostControlKpi.json"];
-var _people = ["人员素质","人员素质结构分布","PeoplewareKpi.json"];
+var _profit = ["盈利能力","2101", "2102", "150", "118","ProfitKpi.json"];
+var _service = ["业务增长","2501", "2502", "2503","ServiceKpi.json"];
+var _operate = ["经营效率","158", "2602","OperatingKpi.json"];
+var _cash = ["现金获取","2301", "103", "2303","GetCashKpi.json"];
+var _debt = ["债务风险","2201", "2202","DebtRiskKpi.json"];
+var _cost = ["成本控制","2401", "2402", "2403", "2404", "126", "120", "2407", "149", "148","CostControlKpi.json"];
+var _people = ["人员素质","2701","PeoplewareKpi.json"];
 function _getKpiSort(kpi) {
     var a = [_people, _profit, _service, _operate, _cash, _debt, _cost];
     for (var i=0;i<a.length;i++) {
--- CWBI/web/scripts/op_water_kpiSort.js
+++ CWBI/web/scripts/op_water_kpiSort.js
@@ -2,13 +2,13 @@
  * 根据kpi的值来获取该kpi的分类
  * @type {string[]}
  */
-var _profit = ["盈利能力","1101", "1102", "1103", "1104", "63", "36","Profit.json"];//
-var _service = ["业务增长","1501", "1502", "1503","1504","Business.json"];//
+var _profit = ["盈利能力","1101", "1102", "1103", "1104", "63", "36","Profit.json"];
+var _service = ["业务增长","1501", "1502", "1503","1504","Business.json"];
 var _operate = ["经营效率","1601", "68", "1603", "1604", "1605","Effeient.json"];
-var _cash = ["现金获取","1301", "3", "1303","Cash.json"];//
-var _debt = ["债务风险","1201", "1202","Debt.json"];//
-var _cost = ["成本控制","34","1402", "1403", "1404", "1405", "44", "38", "1408", "62", "1410","31","Cost.json"];//
-var _people = ["人员素质","1701","Person.json"];//
+var _cash = ["现金获取","1301", "3", "1303","Cash.json"];
+var _debt = ["债务风险","1201", "1202","Debt.json"];
+var _cost = ["成本控制","34","1402", "1403", "1404", "1405", "44", "38", "1408", "62", "1410","31","Cost.json"];
+var _people = ["人员素质","1701","Person.json"];
 function _getKpiSort(kpi) {
     var a = [_people, _profit, _service, _operate, _cash, _debt, _cost];
     for (var i=0;i<a.length;i++) {
